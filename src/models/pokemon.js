31af4550b57ea1f980c50df62e66c000aac9e1a6
JeanGuillaumeZaplana <zaplana.jg@gmail.com>
2022-07-11 17:23:08 +0200

    Maj validateur sur le model pokemon.js (hp et cp)

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -7,23 +7,58 @@ module.exports = (sequelize, DataTypes) => {
         },
         name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: {
+                msg: 'Le nom est déjà pris.'
+            },
+            validate: {
+                len: {
+                    args: [1, 25],
+                    msg: 'Le nom doit contenir entre 1 et 25 caractères.'
+                },
+                notEmpty: { msg: 'Le nom ne peut pas être vide.' },
+                notNull: { msg: 'Le nom est une propriété requise.' }
+            }
         },
         hp: {
             type: DataTypes.INTEGER,
             allowNull: false,
             validate: {
                 isInt: { msg: 'Utilisez uniquement des nombres entiers pour les points de vie.' },
-                notNull: { msg: 'Les oints de vie sont une propriété requise.' }
+                min: {
+                    args: [0],
+                    msg: 'Les points de vie doivent être supérieurs ou égales à 0'
+                },
+                max: {
+                    args: [999],
+                    msg: 'Les points de vie doivent être inférieurs ou égales à 999'
+                },
+                notNull: { msg: 'Les points de vie sont une propriété requise.' }
             }
         },
         cp: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isInt: { msg: 'Utilisez uniquement des nombres entiers pour les points de degâts.' },
+                min: {
+                    args: [0],
+                    msg: 'Les points de dégâts doivent être supérieurs ou égales à 0'
+                },
+                max: {
+                    args: [99],
+                    msg: 'Les points de dégâts doivent être inférieurs ou égales à 99'
+                },
+                notNull: { msg: 'Les points de dégâts sont une propriété requise.' }
+            }
         },
         picture: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isUrl: { msg: 'Utilisez uniquement une URL valide pour l\'image.' },
+                notNull: { msg: 'L\'image est une propriété requise.' }
+            }
         },
         types: {
             type: DataTypes.STRING,
